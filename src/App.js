b40e0436cbfe353a631ba2b960f9801a84722cd6
galactic-shark <evamichael100@gmail.com>
2023-08-06 19:00:37 +0800

    removed unused vars

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Start from "./components/Start";
-import Questions from "./components/Questions";
-import Answers from "./components/Answers";
+
 function App() {
   return (
     <div className="App">
       <Start />
-      {/* <Questions />
-      <Answers /> */}
     </div>
   );
 }
